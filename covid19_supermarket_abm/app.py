bcdce722508c88b75f273ba2a4e1b470fa6a00d6
Alpo Turunen <Alpo.96@gmail.com>
2021-04-26 11:28:52 +0300

    Added more parameters to web app and results.html

--- covid19_supermarket_abm/app.py
+++ covid19_supermarket_abm/app.py
@@ -7,9 +7,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        parameter1 = request.form['content']
-        print(parameter1)
-        return redirect('/')
+        # Set parameters
+        config = {'arrival_rate': request.form['param1'],
+                  'traversal_time': request.form['param2'],
+                  'infection_proportion': request.form['param3'],
+                  "logging_enabled": request.form['param4'],
+                  'day': request.form['param5'],
+                  'customers_together': request.form['param6']}
+        print(config)
+
+        parameters_ok = True
+        if parameters_ok == True:
+            # Run simulation ans show results
+            return render_template('results.html')
+        else:
+            # Re-input parameters
+            return redirect('/')
+
     else:
         return render_template('index.html')
 
